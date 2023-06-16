f3a9b7ba52fc3940554fe213c436da4786635ceb
Wouter Vernaillen <wouter@vernaillen.com>
2023-06-16 16:31:20 +0200

    concept goals & lagon config

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Vernaillen.dev concept website</h1>
+        <h2>Goals:</h2>
+        <ul>
+            <li>Build a markdown driven website with Nuxt 3 and Nuxt Content 2</li>
+            <li>Serverless hosting with Edge Functions (Vervel Edge and/or Lagon)</li>
+            <li>Try to be as lightweight and fast as possible</li>
+        </ul>
+        <h2>Decisions:</h2>
+        <ul>
+            <li>Use UnoCss instead of TailwindCss</li>
+            <li>Avoid Nuxt Icon, adding it seems to push resource usage a lot on Lagon</li>
+        </ul>
+    </div>
+</template>
